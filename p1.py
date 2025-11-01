ea12235bb36e9119a91867d9f74ea1c3f39ab7a5
Faizansid09 <fs216766@gmail.com>
2025-11-01 11:33:02 +0530

    first commit

---
+++ p1.py
@@ -0,0 +1,55 @@
+#To Do List
+task=[]
+
+flag=True
+while flag:
+    print("To do List")
+    print("1. Add a  new list")
+    print("2. View All Existing Task") 
+    print("3. Delete a task")   
+    print("4 .Exit")
+
+    choice=int(input("Enter Choice 1-4:  "))
+    if choice==1:
+            t=input("Enter new Task: ")
+            task.append(t)
+            print("Added Sucessfully")
+    elif choice ==2:
+            if len(task)==0:
+                print("No task available")
+            else:
+                i=1
+                for l in task:
+                    print(i,".",l)
+                    i+=1
+    elif choice ==3:
+         if len(task)==0:
+            print("No task to delete")
+         else:
+            i=1
+            for l in task:
+                    print(i,".",l)
+                    i+=1
+            print("Enter the task to be deleted: ")
+            num=int(input())
+            if num>=1 and num<len(task):
+                      task.pop(num-1)
+                      print("Task deleted sucessfully")
+                      i=1
+                      for l in task:
+                         print(i,".",l)
+                         i+=1
+    elif choice==4:
+          print("Exiting...")
+          flag=False
+    else:
+          ("Enter Valid Choice ")
+
+          
+          
+              
+         
+                    
+
+
+            
